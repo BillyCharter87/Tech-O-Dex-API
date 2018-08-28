002da446b33ac9c0448ff15ed85a8261fa39f2dd
billycharter73 <billycharter73@gmail.com>
2018-08-27 22:28:23 -0400

    Changing to cr

--- scripts/runapp.sh
+++ scripts/runapp.sh
@@ -1,4 +1,1 @@
-#!/bin/bash
-echo Starting my app
-cd /home/ec2-user/Tech-O-Dex/target
-java nohup java -jar Tech-O-Dex-0.1.0.jar
+#!/bin/bashecho Starting my appcd /home/ec2-user/Tech-O-Dex/targetjava nohup java -jar Tech-O-Dex-0.1.0.jar
